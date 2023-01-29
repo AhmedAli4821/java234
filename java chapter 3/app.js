797956350da5d1a8b3f108b17f262c65d65c1772
AhmedAli4621 <ahmadali4821@gmail.comgit statusgit config --global user.email ahmadali4821@gmail.com>
2023-01-29 12:46:58 -0800

    Now ready to commit

---
+++ java chapter 3/app.js
@@ -0,0 +1,11 @@
+var age = "I am 15 years old"
+
+alert(age)
+
+var visit = "You have visited this sit 14 times"
+
+alert (visit)
+
+// document.write ("My year is 1990<br>Data type of my ddeclared verible is number")
+
+document.write (" Sajjad Ahmed ordered 5 T-shirt(s) on XYZ Clothing store.")
---
+++ java chapter4/app.js
@@ -0,0 +1,9 @@
+document.write("<h2>Rules for naming JS variables</h2><p>")
+
+document.write("Variable names can only contain ,numbers,$ and _ .For example: $my_1st varibale<br>")
+
+document.write("Variables must begin with a latter,$ or_ .For example:$name, _name or name<br>")
+
+document.write("Variable names are case sensitive<br>")
+
+document.write("Variable names should not be JS Keywords")
